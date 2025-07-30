3d4f16ea6aedd8505f398b10b42c881b7a8999c6
Hotwheels <jacobs024568@gmail.com>
2025-07-29 21:24:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@
 
 using namespace std;
 
-
-
 const float straightProb = 0.7;
 const float LeftProb = 0.2;
 const float RightProb = 0.1;
@@ -18,11 +16,11 @@ const float RightProb = 0.1;
 const float sensingPath = 0.85;
 const float sensingWall = 0.95;
 
-
 bool inside(int x, int y, int max_x, int max_y) {
 	return (x >= 0 && x < max_x && y >= 0 && y < max_y);
 }
 
+vector<bool> sensing(vector<bool>& sensed);
 void execute(vector<vector<Cell>>& maze, Position goal);
 void print(const vector<vector<Cell>>& maze);
 
@@ -35,26 +33,31 @@ int main() {
 		{Cell::Path(),	Cell::Wall(),	Cell::Path(),	Cell::Path(),	Cell::Path(),	Cell::Path()},
 		{Cell::Path(),	Cell::Path(),	Cell::Path(),	Cell::Path(),	Cell::Path(),	Cell::Path()}
 	};
+
 	print(maze);
-	//Position start = locate(CellType::Start, maze);
 	Position goal = locate(CellType::Goal, maze);
 	cout << "Goal: " << goal.x << ",  " << goal.y << endl;
+
 	execute(maze, goal);
-	cout << Direction::East.Left() << endl;
+	//cout << Direction::East.Left() << endl;
 	return 0;
 }
 
 vector<bool> sensing(vector<bool>& sensed) {
 	int size = (int)sensed.size();
 	vector<bool> actuallySensed(size);
+	int chance;
+
 	for (int i = 0; i < size; ++i) {
-		int chance = (rand() % 100) + 1; // 1 to 100
+		chance = (rand() % 100) + 1; // 1 to 100
 		bool evaluated = sensed[i] ? chance <= (1 - sensingPath) * 100 : chance <= (1 - sensingWall) * 100;
+
 		if (evaluated)
 			actuallySensed[i] = !sensed[i];
 		else
 			actuallySensed[i] = sensed[i];
 	}
+
 	return actuallySensed;
 }
 
@@ -75,26 +78,26 @@ void execute(vector<vector<Cell>>& maze, Position goal) {
 				maze[i][j].label = prob;
 
 	/*
-	//1. Sensing: [0,0,0,1] //Filtering after Evidence
-	sensed = sensing(true, true, true, false);
+	    //1. Sensing: [0,0,0,1] //Filtering after Evidence
+	    sensed = sensing(true, true, true, false);
 
-	//2. Moving north-ward  //Windy Movement Probability
-	movedDirection = windyMove(Direction::North);
+	    //2. Moving north-ward  //Windy Movement Probability
+	    movedDirection = windyMove(Direction::North);
 
-	//3. Sensing: [1,0,0,0] //Filtering after Evidence
-	sensed = sensing(false, true, true, true);
+	    //3. Sensing: [1,0,0,0] //Filtering after Evidence
+	    sensed = sensing(false, true, true, true);
 
-	//4. Moving north-ward  //Windy Movement Probability
-	movedDirection = windyMove(Direction::North);
+	    //4. Moving north-ward  //Windy Movement Probability
+	    movedDirection = windyMove(Direction::North);
 
-	//5. Sensing: [1,1,0,0] //Filtering after Evidence
-	sensed = sensing(false, false, true, true);
+	    //5. Sensing: [1,1,0,0] //Filtering after Evidence
+	    sensed = sensing(false, false, true, true);
 
-	//6. Moving east-ward   //Windy Movement Probability
-	movedDirection = windyMove(Direction::East);
+	    //6. Moving east-ward   //Windy Movement Probability
+	    movedDirection = windyMove(Direction::East);
 
-	//7. Sensing: [0,1,1,0] //Filtering after Evidence
-	sensed = sensing(true, false, false, true);
+	    //7. Sensing: [0,1,1,0] //Filtering after Evidence
+	    sensed = sensing(true, false, false, true);
 	*/
 }
 
@@ -107,11 +110,9 @@ void print(const vector<vector<Cell>>& maze) {
 			const Cell& temp = maze[i][j];
 			if (temp.type == CellType::Wall)
 				cout << setw(6) << right << "#####";
-
-			else {
+			else
 				// Print probability (label) formatted as "--.--"
 				cout << setw(6) << right << fixed << setprecision(2) << temp.label;
-			}
 		}
 		cout << endl;
 	}
